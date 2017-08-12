1719bde78fe52ed1af3afaba2ea16b83cd3b865a
FerneyP35 <ferneyp29@gmail.com>
2017-08-12 12:59:11 -0500

    clase 12 agosto

---
+++ modulo_3/lab4/index.js
@@ -0,0 +1,8 @@
+var a;
+a = 'Variable';
+var b = 5;
+var c = true;
+var d = ['a', 'b', 'c'];
+console.log(a + '\n' + b + '\n' + c + '\n');
+console.log(d);
+console.log(typeof d);
---
+++ modulo_3/lab4/index.ts
@@ -0,0 +1,12 @@
+let a:string;
+a = 'Variable';
+
+let b:number = 5;
+
+let c:boolean = true;
+
+let d:Array<string> = ['a', 'b', 'c'];
+
+console.log(a + '\n' + b + '\n' + c + '\n');
+console.log(d);
+console.log(typeof d);

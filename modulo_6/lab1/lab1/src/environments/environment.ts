35dfb30f0aa71000e54a9f70d19d5b3d5dfef05d
FerneyP35 <ferneyp29@gmail.com>
2017-09-09 12:53:29 -0500

    Clase 9 septiembre
    Clase 9 de septiembre.

---
+++ modulo_6/lab1/lab1/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false, 
+  firebase: 
+  {
+  	apiKey: "AIzaSyDxl5Rl6oTgdIoRHNmrkJPAcpJzECIfNEo",
+    authDomain: "apm-6-bc4ac.firebaseapp.com",
+    databaseURL: "https://apm-6-bc4ac.firebaseio.com",
+    projectId: "apm-6-bc4ac",
+    storageBucket: "apm-6-bc4ac.appspot.com",
+    messagingSenderId: "679022556352"
+  }
+};
